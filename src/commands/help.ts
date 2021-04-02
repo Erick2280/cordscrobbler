bc2d11004fcebb629f8827dc157e620364a9d201
Erick Almeida <erickingles@gmail.com>
2021-04-02 02:05:00 -0300

    Update help command output to use embed

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,5 +1,6 @@
 import { Message, TextChannel } from 'discord.js';
 import fs from 'fs';
+import { composeBasicMessageEmbed } from '../utils';
 
 export const data = {
     name: 'help',
@@ -8,32 +9,33 @@ export const data = {
     args: false
 };
 
-export function execute(message: Message) {
+export async function execute(message: Message) {
 
     let messageText =
-`Cordscrobbler v${process.env.NPM_PACKAGE_VERSION ?? require('../lib/version.js')}
+`**What is this bot?**
 
-__**What is this bot?**__
 This bot scrobbles songs played by other bots on your Discord server to Last.fm. I will automatically scrobble if you are on the same audio channel as the bot, on any server that I'm added to.
-To enable it for you, you'll need to send a message to me with **${process.env.DISCORD_BOT_PREFIX}register** and log in with your Last.fm account.
+To enable it for you, you'll need to send a message to me with \`${process.env.DISCORD_BOT_PREFIX}register\` and log in with your Last.fm account.
 
-Some bots require additional configuration. Please send **${process.env.DISCORD_BOT_PREFIX}supportedbots** for further help on this.
+Some bots require additional configuration. Please send \`${process.env.DISCORD_BOT_PREFIX}supportedbots\` for further help on this.
 
-__**Commands**__`;
+**Commands**\n`;
     const commandsFolder = __dirname;
     const commandFiles = fs.readdirSync(commandsFolder).filter(file => file.endsWith('.js') || file.endsWith('.ts'));
     
     for (const file of commandFiles) {
         const command = require(`${commandsFolder}/${file}`);
-            messageText += `\n**${process.env.DISCORD_BOT_PREFIX}${command.data.name}**`;
+            messageText += `\n\`${process.env.DISCORD_BOT_PREFIX}${command.data.name}`;
             if (command.data.usage) {
-                messageText += ` _${command.data.usage}_`;
+                messageText += ` ${command.data.usage}\``;
+            } else {
+                messageText += `\``
             }
             if (command.data.aliases) {
                 messageText += `\n_it can also be called using `;
                 let index = 0;
                 for (const alias of command.data.aliases) {
-                    messageText +=`**${process.env.DISCORD_BOT_PREFIX}${alias}**`;
+                    messageText +=`\`${process.env.DISCORD_BOT_PREFIX}${alias}\``;
                     if (index !== command.data.aliases.length - 1) {
                         messageText += ', ';
                     }
@@ -45,10 +47,15 @@ __**Commands**__`;
             messageText += `\n• ${command.data.description}\n`;
     }
 
-    messageText += `\nI'm open source! Visit my GitHub project page on https://github.com/Erick2280/cordscrobbler. If you're facing any issues, you can report them on https://github.com/Erick2280/cordscrobbler/issues.`
+    messageText += `\nI'm open source! Visit my GitHub project page on https://github.com/Erick2280/cordscrobbler. If you're facing any issues, you can report them on [GitHub](https://github.com/Erick2280/cordscrobbler/issues) or on [Cordscrobbler Discord server](https://discord.gg/yhGhQj6cGa).`
+
+    const versionFooter = `Cordscrobbler v${process.env.NPM_PACKAGE_VERSION ?? require('../lib/version.js')}`
+
+    const messageEmbed = await composeBasicMessageEmbed('Help', messageText, versionFooter)
 
     if (message.channel instanceof TextChannel) {
         message.reply('I sent the help via DM :)')
     }
-    message.author.send(messageText);
+
+    message.author.send(messageEmbed);
 }
